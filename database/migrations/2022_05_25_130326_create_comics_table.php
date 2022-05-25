89994262eec1b24c63248de10d16c62089b6d5ea
Maria210194 <mary.stachie@gmail.com>
2022-05-25 15:35:34 +0200

    create model and controller

---
+++ database/migrations/2022_05_25_130326_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->text('description')->nullable();
+            $table->string('thumb', 100);
+            $table->unsignedSmallInteger('price');
+            $table->string('serie', 100);
+            $table->time('sale_date')->nullable($value=true);
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
